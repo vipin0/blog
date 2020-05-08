1ef00282dd6bf760363805f4fb7e77fcc8ad8d5a
Manikant Jaiswal <vipin6673@gmail.com>
2020-05-08 18:50:53 +0530

    w3.css

--- myblog/models.py
+++ myblog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
-    file = models.FileField(upload_to='documents/%Y/%m/%d', default=None)
+    file = models.FileField(upload_to='documents/%Y/%m/%d', default=None )
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
